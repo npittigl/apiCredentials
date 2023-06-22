956a8097238bc317f94c1292a2509a2971ee4579
npittigl <npittigl@gmail.com>
2023-06-22 18:46:09 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { useEffect } from 'react'
+
+const App = () => {
+  // Creating a function that performs a network request to the Rijks Museum
+  const fetchRijksData = () => {
+    const url = new URL('https://www.rijksmuseum.nl/api/en/collection/');
+    // Adding the param values to our url
+    url.search = new URLSearchParams({
+      key: process.env.REACT_APP_API_KEY
+    });
+    // Using the fetch function to get the information
+    fetch(url).then((response) => {
+      return response.json()
+    }).then((data) => {
+      console.log(data)
+    });
+  }
+  // This runs the code as soon as the component is rendered to the page
+  useEffect(() => {
+    fetchRijksData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
-  );
+  )
 }
 
 export default App;
